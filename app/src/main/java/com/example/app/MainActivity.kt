dfd042777da3169e49b3864a8810fc8722df989c
mrigua <mrigua@gmail.com>
2026-01-01 00:43:38 +0100

    Add MainActivity with WebView implementation

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        setContentView(webView)
+
+        val siteUrl = "PLACEHOLDER_URL" 
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl(siteUrl)
+    }
+}
